9bf9746725902e87af4808c4f35ce640166690c7
Shengjing Zhang <szhang040@e.ntu.edu.sg>
2020-06-22 00:06:31 +0800

    Add translation API

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -30,6 +30,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap; 
 import java.util.Map; 
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
@@ -64,6 +68,21 @@ private ArrayList<String> messages = new ArrayList<>();
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Get the request parameters.
+    String originalText = request.getParameter("text");
+    String languageCode = request.getParameter("languageCode");
+
+    // Do the translation.
+    Translate translate = TranslateOptions.getDefaultInstance().getService();
+    Translation translation =
+        translate.translate(originalText, Translate.TranslateOption.targetLanguage(languageCode));
+    String translatedText = translation.getTranslatedText();
+
+    // Output the translation.
+    response.setContentType("text/html; charset=UTF-8");
+    response.setCharacterEncoding("UTF-8");
+    response.getWriter().println(translatedText);
+    
     // Get the input from the form.
     String comments = request.getParameter("comment-input");
 
@@ -79,9 +98,9 @@ private ArrayList<String> messages = new ArrayList<>();
     response.getWriter().println(comments);
     response.getWriter().println("<br/><br/>");
     response.getWriter().println("Thanks for your comment!");
-
-    // Redirect back to the HTML page.
-    //response.sendRedirect("/index.html");
   }
+
+  
 }
+
 
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -51,5 +51,22 @@ function createListElement(text) {
   liElement.innerText = text;
   return liElement;
 }
+function requestTranslation() {
+        const text = document.getElementById('text').value;
+        const languageCode = document.getElementById('language').value;
+        const resultContainer = document.getElementById('result');
+        resultContainer.innerText = 'Loading...';
 
+        const params = new URLSearchParams();
+        params.append('text', text);
+        params.append('languageCode', languageCode);
+
+        fetch('/data', {
+          method: 'POST',
+          body: params
+        }).then(response => response.text())
+        .then((translatedMessage) => {
+          resultContainer.innerText = translatedMessage;
+        });
+      }
 
